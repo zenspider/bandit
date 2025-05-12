aa7a5070a4e8668ecc7a87332b3d95e00f9e85cf
Ryan Davis <ryand-ruby@zenspider.com>
2025-05-12 13:39:55 -0700

    Added Bandit::FW::IpSet

--- lib/bandit/fw.rb
+++ lib/bandit/fw.rb
@@ -4,6 +4,7 @@ class Bandit
     class Abstract
       def ban(ip)   = raise NotImplementedError, "#{__method__} not implemented"
       def unban(ip) = raise NotImplementedError, "#{__method__} not implemented"
+      def start(ips) = raise NotImplementedError, "#{__method__} not implemented"
     end
 
     class Logger < Abstract
@@ -16,6 +17,38 @@ class Bandit
       def unban(ip)       = log "UNBAN %s", ip
       def start(ips)      = log "START BAN %d IPS", ips.size
     end
+
+    class IpSet < Logger
+      def _restore(&b)
+        IO.popen "ipset restore", "w", &b
+      end
+
+      def ban ip
+        super
+
+        _restore do |io|
+          io.puts "add bandit %s" % [ip]
+        end
+      end
+
+      def unban ip
+        super
+
+        _restore do |io|
+          io.puts "del bandit %s" % [ip]
+        end
+      end
+
+      def start ips
+        super
+
+        _restore do |io|
+          io.puts "create bandit hash:ip -exist"
+          io.puts "create bandit_tmp hash:ip"
+          io.puts ips.map { |ip| "add bandit_tmp #{ip}" }
+          io.puts "swap bandit_tmp bandit"
+          io.puts "destroy bandit_tmp"
+        end
       end
     end
   end
